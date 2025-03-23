1be5b1a96e7ab91bdcbcadd4aaf4c948ec81f14a
Tatyana Lapshina <lapshinatatyana25@mail.ru>
2025-03-23 23:42:27 +0300

    Сreating pages, navigation, and footer

--- Confectionary/views.py
+++ Confectionary/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main_page(request):
+    return render(request, 'main_page.html')
+
+def showcase(request):
+    return render(request, 'showcase.html')
+
+def photo_album(request):
+    return render(request, 'photo_album.html')
+
+def custom_cakes(request):
+    return render(request, 'custom_cakes.html')
+
+def delivery(request):
+    return render(request, 'delivery.html')
+
+def employees(request):
+    return render(request, 'employees.html')
+
+def trolley(request):
+    return render(request, 'trolley.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
